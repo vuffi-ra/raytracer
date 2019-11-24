e6fa617c1776857e6b83d0bb6e63a7e6be90ac04
Markus Muegge <markus.muegge@t-online.de>
2019-11-24 21:25:28 +0100

    Add support for different materials

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,12 +13,16 @@ data Color24 = RGB24 (V3 Word8)
 
 data Ray = Ray (V3 Double) (V3 Double)
 
-data Shape = Sphere (V3 Double) Double
+data Albedo = Albedo (V3 Double)
+data Material = Diffuse Albedo
+
+data Shape = Sphere (V3 Double) Double Material
 
 data Intersection = Intersection
     { intersectParam :: Double
     , intersectPoint :: V3 Double
     , intersectNormal :: V3 Double
+    , intersectMaterial :: Material
     }
 
 data Camera = Camera
@@ -27,6 +31,8 @@ data Camera = Camera
     , cameraHorizontal :: V3 Double
     , cameraVertical :: V3 Double
     }
+
+data AntiAliasingPattern = Pattern [(Double, Double)]
 
 -- A sphere with radius r around the origin is described by x*x + y*y + z^z = r *r.
 -- A sphere with radius r at cx, cy, cz is described by (x - cx)^2 + (y - cy)^2 + (z - cz)^2 = r^2
@@ -47,13 +53,13 @@ data Camera = Camera
 --
 -- Returns the t of the closest intersection
 intersects :: Ray -> Shape -> Maybe Intersection
-intersects r@(Ray origin direction) (Sphere center radius) = 
-    if discriminant >= 0 && closestPointAlongRay > 0 then
+intersects r@(Ray origin direction) (Sphere center radius material) = 
+    if discriminant >= 0 && closestPointAlongRay > 0.001 then
         let
             point = r `at` closestPointAlongRay
             normal = unit $ (point - center)
         in
-            Just $ Intersection closestPointAlongRay point normal
+            Just $ Intersection closestPointAlongRay point normal material
     else
         Nothing
     where
@@ -73,8 +79,8 @@ closestIntersection r ss =
         [] -> Nothing
         xs -> Just $ minimumBy (\a b -> compare (intersectParam a) (intersectParam b)) xs
 
-raysFromPixel :: Camera -> (Int, Int) -> [(Double, Double)] -> (Int, Int) -> [Ray]
-raysFromPixel c (width, height) offsets (x, y) = let
+raysFromPixel :: Camera -> (Int, Int) -> AntiAliasingPattern -> (Int, Int) -> [Ray]
+raysFromPixel c (width, height) (Pattern offsets) (x, y) = let
     x' = fromIntegral x
     y' = fromIntegral y
     width' = fromIntegral width
@@ -89,65 +95,91 @@ raysFromPixel c (width, height) offsets (x, y) = let
             corner = cameraLowerLeftCorner c
     in fmap ray $ uvs
 
-chunks :: Int -> [a] -> [[a]]
-chunks _ [] = []
-chunks 0 xs = [xs]
-chunks n xs = chunk : chunks n rest where
-    (chunk, rest) = splitAt n xs
+randomAAPatterns :: RandomGen g => g -> Int -> AntiAliasingPattern
+randomAAPatterns g n = Pattern $ take n (zip (randoms g1) (randoms g2)) where
+    (g1, g2) = split g
 
 raytracer :: IO ()
 raytracer = let
-        width = (200 :: Int)
-        height = (100 :: Int)
+        width = (400 :: Int)
+        height = (200 :: Int)
         camera = Camera (V3 0 0 0) (V3 (-2) (-1) (-1)) (V3 4 0 0) (V3 0 2 0)
-        spheres = [ (Sphere (V3 0 0 (-1)) 0.5), (Sphere (V3 0 (-100.5) (-1)) 100) ]
-        raysPerPixel = (100 :: Int)
+        material = Diffuse (Albedo (V3 0.5 0.5 0.5))
+        spheres = [ (Sphere (V3 0 0 (-1)) 0.5) material, (Sphere (V3 0 (-100.5) (-1)) 100) material ]
+        raysPerPixel = (20 :: Int)
         -- Reversed, so that the order of pixels aligns with the order of pixels
         -- in the output file
         rows = [height - 1, height - 2 .. 0]
         cols = [0 .. width - 1]
         pixels = [ (x, y) | y <- rows, x <- cols ]
-        innerTrace = (flip trace) spheres
         rays = raysFromPixel camera (width, height)
+        patternGen = (flip randomAAPatterns) raysPerPixel
     in do
         rng <- getStdGen
-        let (rng1, rng2) = split rng
-        let offsets = chunks raysPerPixel $ zip (randoms rng1) (randoms rng2)
-        let offsetRays = fmap rays offsets
-        let rayGroups = zipWith ($) offsetRays pixels
-        let colors = fmap (\rayGroup -> averageColor $ fmap innerTrace rayGroup) rayGroups
-        let header = "P3\n200 100\n255\n"
-        let content = concat $ (fmap colorToString colors)
+        let (aaRng, traceRng) = split rng
+        let aaRngs = unfoldr (\a -> Just $ split a) aaRng
+        let traceRngs = unfoldr (\a -> Just $ split a) traceRng
+        let aaPatterns = patternGen <$> aaRngs
+        let rayGroups = zipWith3 ($) (repeat rays) aaPatterns pixels
+        let colors = averageColor <$> (zipWith4 ($) (repeat traceRays) traceRngs (repeat spheres) rayGroups)
+        let header = "P3\n" ++ (show width) ++ " " ++ (show height) ++ "\n255\n"
+        let content = concat $ (fmap (colorToString . gammaCorrection) colors)
         writeFile "/home/markus/out.ppm" (header ++ content)
 
-colorToString :: Color -> String
-colorToString c = (show r) ++ " " ++ (show g) ++ " " ++ (show b) ++ " "
-    where (RGB24 (V3 r g b)) = toPPM c
+colorToString :: V3 Double -> String
+colorToString c = (show r) ++ " " ++ (show g) ++ " " ++ (show b) ++ " " where 
+    V3 r g b = toPPM c
+    toPPM :: V3 Double -> V3 Word8
+    toPPM v = toWord8 <$> (255.0 .* v) where
+        toWord8 :: Double -> Word8
+        toWord8 = fromIntegral . (round :: Double -> Integer)
 
-toPPM :: Color -> Color24
-toPPM (RGB v) = RGB24 $ toWord8 <$> (255.0 .* v) where
-    toWord8 :: Double -> Word8
-    toWord8 = fromIntegral . (round :: Double -> Integer)
+gammaCorrection :: V3 Double -> V3 Double
+gammaCorrection = fmap sqrt
 
-averageColor :: [Color] -> Color
-averageColor cs = RGB $ (combined /. (fromIntegral $ Data.List.length cs)) where
-    combined = sum $ fmap (\(RGB v) -> v) cs
+averageColor :: [V3 Double] -> V3 Double
+averageColor vs = combined /. (fromIntegral $ Data.List.length vs) where
+    combined = sum vs
 
 at :: Ray -> Double -> V3 Double
 at (Ray o d) t = o + t .* d
 
-backgroundGradient :: Ray -> Color
-backgroundGradient (Ray _ d) = RGB $ (1 - t) .* white + t .* blue
+backgroundGradient :: Ray -> V3 Double
+backgroundGradient (Ray _ d) = (1 - t) .* white + t .* blue
     where
         white = V3 1 1 1
         blue = V3 0.5 0.7 1
         (V3 _ y _) = unit d
         t = 0.5 * (y + 1)
 
-trace :: Ray -> [Shape] -> Color
-trace r ss = 
-    maybe background (normalToColor . intersectNormal) $ (closestIntersection r ss)
-    where
-        background = backgroundGradient r
-        normalToColor n = RGB $ 0.5 .* ((V3 1 1 1) + n)
+-- Via https://math.stackexchange.com/a/1586185
+randomInUnitSphere :: RandomGen g => g -> V3 Double
+randomInUnitSphere g = V3 x y z where
+    a, b :: Double
+    (a, g') = random g 
+    (b, _) = random g'
+    lambda = acos (2 * a - 1) - (pi / 2)
+    theta = 2 * pi * b
+    x = (cos lambda) * (cos theta)
+    y = (cos lambda) * (sin theta)
+    z = sin lambda
+
+traceRays :: RandomGen g => g -> [Shape] -> [Ray] -> [V3 Double]
+traceRays g shapes rays = 
+    let
+        rngs = unfoldr (\a -> Just $ split a) g
+    in
+        zipWith4 ($) (repeat traceRay) rngs (repeat shapes) rays
+
+traceRay :: RandomGen g => g -> [Shape] -> Ray -> V3 Double
+traceRay g ss r = case closestIntersection r ss of
+    Nothing -> backgroundGradient r
+    Just (Intersection _ p _ m) -> case m of
+        Diffuse (Albedo albedo) -> 
+            let 
+                (g', g'') = split g
+                target = randomInUnitSphere g'
+                ray = Ray p (target - p)
+            in
+                albedo * traceRay g'' ss ray
 
